8489d83a40041cd1dfb7ac7d97b7b10cd56817d1
Guillermo Alvarez <guillermo@icalialabs.com>
2021-06-15 11:32:57 -0600

    init

---
+++ lib/rick_morty_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rick_morty_api/version"
+
+module RickMortyApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rick_morty_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RickMortyApi do
+  it "has a version number" do
+    expect(RickMortyApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
